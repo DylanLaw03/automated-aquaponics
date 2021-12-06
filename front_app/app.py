0994f577d5e8b7e17fbd960a5efb1d474b9a0e01
DylanLaw03 <dalawren@uvm.edu>
2021-12-06 13:21:11 -0500

    replaced liveTemp.html with index.html. Cleaned up code

--- front_app/app.py
+++ front_app/app.py
@@ -4,13 +4,15 @@ from flask_bootstrap import Bootstrap
 app = Flask(__name__)
 Bootstrap(app)
 
-@app.route('/livetemp')
-def index():
-    return render_template('index.html')
-
 @app.route('/')
 def liveTemp():
-    return render_template('liveTemp.html')
+    return render_template('index.html')
+
+@app.route('/history')
+def history():
+    return render_template('history.html', database = "Test")
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug = True)
+
