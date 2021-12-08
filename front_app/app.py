8eccf9a5eaab815801626f2f16afc052c7b1d138
Evan Hinchliffe <90803611+ehinchli@users.noreply.github.com>
2021-12-07 20:34:46 -0500

    Temp log frontend

--- front_app/app.py
+++ front_app/app.py
@@ -5,6 +5,8 @@ Evan Hinchliffe
 """
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+import mysql.connector
+import json
 
 app = Flask(__name__)
 Bootstrap(app)
@@ -15,9 +17,26 @@ def liveTemp():
     return render_template('index.html')
 
 
+credentials = json.load(open("../back_process/credentials.json", "r"))
+
+
 @app.route('/history')
 def history():
-    return render_template('history.html', database="Test")
+    database = mysql.connector.connect(
+        host=credentials["host"],
+        user=credentials["user"],
+        passwd=credentials["password"],
+        database=credentials["database"]
+    )
+    cursor = database.cursor()
+    query = 'SELECT * FROM temperature_data;'
+
+    cursor.execute(query)
+    data = cursor.fetchall()
+
+    cursor.close()
+    database.close()
+    return render_template('history.html', database="aquaponics", data = data,)
 
 
 @app.route('/status')
